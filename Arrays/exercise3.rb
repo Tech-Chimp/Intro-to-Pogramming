82d86424ce20a3efee91e0e0280c94ccb400d9aa
Tim <tim.mikaila@outlook.com>
2019-11-08 17:34:55 +0000

    V1

---
+++ Arrays/exercise3.rb
@@ -0,0 +1,7 @@
+arr = [["test", "hello", "world"],["example", "mem"]] #2D array
+
+print arr[1][0]
+
+#or
+
+print arr.last.first
---
+++ Arrays/exercise7.rb
@@ -0,0 +1,7 @@
+array1 = [4, 9, 16, 25, 36, 49]
+
+array2 = array1.map { |num| num + 2 }
+
+p array1
+p array2
+
